fa4e917efab818e6e2f75c01c32fbab7ec7e835d
Nick Ustinov <nickustinov@gmail.com>
2026-02-24 14:05:39 +0000

    Show hide/show label in menu bar based on window state

--- Sources/App/AppDelegate.swift
+++ Sources/App/AppDelegate.swift
@@ -204,11 +204,9 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuItemValidation, NSTool
     }
 
     @objc private func statusItemClicked(_ sender: NSStatusBarButton) {
-        // If window is hidden or not key, show it directly
         if let window = editorWindow, !window.isVisible || !window.isKeyWindow {
             showItsypad()
         } else {
-            // Window is visible and active, show menu
             showMenu()
         }
     }
@@ -216,9 +214,13 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuItemValidation, NSTool
     private func showMenu() {
         let menu = NSMenu()
 
-        let showItsypadTitle = String(localized: "statusbar.show", defaultValue: "Show Itsypad")
-        let showItem = NSMenuItem(title: showItsypadTitle, action: #selector(showItsypad), keyEquivalent: "")
-        showItem.image = NSImage(systemSymbolName: "macwindow", accessibilityDescription: nil)
+        let windowIsActive = editorWindow?.isKeyWindow == true
+        let toggleTitle = windowIsActive
+            ? String(localized: "statusbar.hide", defaultValue: "Hide Itsypad")
+            : String(localized: "statusbar.show", defaultValue: "Show Itsypad")
+        let toggleIcon = windowIsActive ? "macwindow.badge.minus" : "macwindow"
+        let showItem = NSMenuItem(title: toggleTitle, action: #selector(showItsypad), keyEquivalent: "")
+        showItem.image = NSImage(systemSymbolName: toggleIcon, accessibilityDescription: nil)
         showItem.target = self
         if let keys = SettingsStore.shared.shortcutKeys {
             if keys.isTripleTap, let mod = keys.tapModifier {
@@ -230,7 +232,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuItemValidation, NSTool
                 else { symbol = "" }
                 let side = mod.hasPrefix("left-") ? " L" : mod.hasPrefix("right-") ? " R" : ""
                 let hint = "  \(symbol)\(symbol)\(symbol)\(side)"
-                let attributed = NSMutableAttributedString(string: showItsypadTitle)
+                let attributed = NSMutableAttributedString(string: toggleTitle)
                 attributed.append(NSAttributedString(string: hint, attributes: [
                     .foregroundColor: NSColor.tertiaryLabelColor,
                 ]))
